5b016f17d399a76c7c04f28c8518843faf9e0937
JonasPedersen123 <jopede23@student.aau.dk>
2024-03-23 18:15:10 +0100

    Added code in routing for better visual represntat -ion of the url for the clint

--- routing.js
+++ routing.js
@@ -7,18 +7,35 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const routing = function(app) {
-    app.get('/', async (req, res) => { // This doesn't work for some reason, fix
+    app.get('/', async (req, res) => { // This doesn't work for some reason, fix,
         console.log(req.session.token);
         console.log(typeof(req.session.token));
         if (req.session.token) {
-            res.redirect('/html/schedule.html');
+            res.redirect('/schedule');
         } 
         else {
-            res.redirect('/html/login.html');
+            res.redirect('/login');
         }    
     });
-    app.get('/favicon.ico', (req, res) => { // And this doesn't work for some reason, fix
-        console.log(path.join('Called favicon',__dirname, '/public/img/favicon.ico'));
+    app.get('/login', (req, res) => {
+        // Send the /html/login.html file when /login is accessed
+        res.sendFile(path.join(__dirname, 'public', 'html', 'login.html'));
+    });
+    app.get('/schedule', (req, res) => {
+        // Send the /html/schedule.html file when /schedule is accessed
+        res.sendFile(path.join(__dirname, 'public', 'html', 'schedule.html'));
+/*      if (req.session.token) {
+            res.sendFile(path.join(__dirname, 'public', 'html', 'schedule.html')); // Doesn't work here either, fix,
+        } 
+        else {
+            res.redirect('/login');
+        } */
+    });
+    app.get('/settings', (req, res) => {
+        // Send the /html/settings.html file when /settings is accessed
+        res.sendFile(path.join(__dirname, 'public', 'html', 'settings.html'));
+    });
+    app.get('/favicon.ico', (req, res) => { // And this doesn't work for some reason, nevermind for some reason it just decided to work
         res.sendFile(path.join(__dirname, '/public/img/favicon.ico'))
     });
     app.get('/MoodleAPI', async (req, res) => {
