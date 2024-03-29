f37937bd7eb9136e1a6cea249a0236053da8d8d1
Allan Krog <143946881+AllankrogAAU@users.noreply.github.com>
2024-03-29 10:48:04 +0100

    DisplayProfile update

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -31,8 +31,22 @@ async function scheduleInitialization() {
 }
 
 function displayProfile(profile) {
-  $("#navbar").append($(`<div id="user_profile">`).append(`<p> Welcome back ${profile.fullname}</p><img src="${profile.userpictureurl}" alt="Profile pic">`));
+  $("#user_profile").html(`<p>Welcome back ${profile.fullname}</p><img src="${profile.userpictureurl}" alt="Profile pic">`);
+
+  if (profile.courses && profile.courses.length > 0) {
+    profile.courses.forEach(course => {
+      const courseLecturesList = $(`<div class="course-lectures"><h3>${course.fullname} Lectures:</h3><ul></ul></div>`);
+      course.contents.forEach(lecture => {
+        courseLecturesList.find("ul").append(`<li>${lecture.name}</li>`);
+      });
+      $("#courses").append(courseLecturesList);
+    });
+  } else {
+    $("#courses").append("<p>You are not enrolled in any courses.</p>");
+  }
 }
+
+
 
 applyTheme();
 scheduleInitialization();  
