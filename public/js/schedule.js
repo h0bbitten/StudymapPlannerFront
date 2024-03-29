ca5ab256e81181fdc5452bc2a7869aaf3f7047b2
Allan Krog <143946881+AllankrogAAU@users.noreply.github.com>
2024-03-29 11:16:51 +0100

    Lectures vises i skema

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -33,6 +33,15 @@ async function scheduleInitialization() {
 function displayProfile(profile) {
   $("#user_profile").html(`<p>Welcome back ${profile.fullname}</p><img src="${profile.userpictureurl}" alt="Profile pic">`);
 
+  const lectureNames = [];
+  profile.courses.forEach(course => {
+    course.contents.forEach(lecture => {
+      lectureNames.push(lecture.name);
+    });
+
+  });
+  localStorage.setItem('lectureNames', JSON.stringify(lectureNames));
+
   if (profile.courses && profile.courses.length > 0) {
     profile.courses.forEach(course => {
       const courseLecturesList = $(`<div class="course-lectures"><h3>${course.fullname} Lectures:</h3><ul></ul></div>`);
@@ -45,7 +54,6 @@ function displayProfile(profile) {
     $("#courses").append("<p>You are not enrolled in any courses.</p>");
   }
 }
-
 
 
 applyTheme();
