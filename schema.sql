832fe1019d3c4ada531222f8852cf3ffe2fd6a2b
FalseViking <83270070+FalseViking@users.noreply.github.com>
2024-04-04 09:33:02 +0200

    Schema changes

--- schema.sql
+++ schema.sql
@@ -3,9 +3,8 @@ USE users;
 
 CREATE TABLE users (
     id integer PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(255) NOT NULL,
     token VARCHAR(255) NOT NULL,
-    course VARCHAR(255) NOT NULL,
     ects DECIMAL (5.1) NOT NULL,
+    schedule VARCHAR(255) NOT NULL,
     created TIMESTAMP NOT NULL DEFAULT NOW()
 );
