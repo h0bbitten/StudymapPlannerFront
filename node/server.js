96c88c94097bc1ff9577f7ea8692adaa30879857
JonasPedersen123 <jopede23@student.aau.dk>
2024-04-13 21:25:24 +0200

    Todo file

--- node/server.js
+++ node/server.js
@@ -31,15 +31,3 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   routing(app);
 });
-
-/*
- *
- * TODO:
- *      -We have two package.json files in the project,
- *          one in the root directory and one in the client directory.
- *      -Webscraper is not working correctly.
- *
- *
- *
- *
- */
