ee05c0f4a92a1989778f0ef9276d9b5f89199da6
FalseViking <83270070+FalseViking@users.noreply.github.com>
2024-04-16 15:17:29 +0200

    Update database.js

--- node/database.js
+++ node/database.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 
 // Database connection pool
 const pool = mysql.createPool({
-  host: 'mysql',
+  host: 'mysql-dock',
   user: 'root',
   password: 'rootyroot',
   database: 'users',
